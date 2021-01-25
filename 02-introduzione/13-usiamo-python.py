4f20ff68088344962c992e6d8321231d775bfef7
atrox3d <atrox3d@gmail.com>
2021-01-25 13:13:49 +0100

    fix tabs

--- 02-introduzione/13-usiamo-python.py
+++ 02-introduzione/13-usiamo-python.py
@@ -1,10 +1,11 @@
 def paridispari():
-     inp = input("Inserisci un numero: ")
-     numero = int(inp)
-     modulo = numero % 2
-     if modulo == 0:
-             print("numero pari")
-     else:
-             print("numero dispari")
+    inp = input("Inserisci un numero: ")
+    numero = int(inp)
+    modulo = numero % 2
+    if modulo == 0:
+        print("numero pari")
+    else:
+        print("numero dispari")
+
 
 paridispari()
