5ec90ee84a6f7f6a1236b52bdb9e8b4c0243031d
atrox3d <atrox3d@gmail.com>
2021-01-26 05:38:41 +0100

    add 23-stringhe-multiline.py

---
+++ 04-basic-data-types/23-stringhe-multiline.py
@@ -0,0 +1,8 @@
+print("si può scrivere una stringa\
+ stringa multiline con un backslash\
+    ma non si può indentare, pena tabs in output")
+
+print("""Si può anche utilizzare una tripla di apici
+e andare a capo quando si vuole
+    anche qui l'indentazione appare in output
+""")
