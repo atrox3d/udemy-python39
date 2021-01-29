d6e3da9d770aebf3d140a06358621bb655966a0f
atrox3d <atrox3d@gmail.com>
2021-01-29 20:44:21 +0100

    add GestoreContiCorrenti

--- 08-esercitazione-object-oriented/conto1.py
+++ 08-esercitazione-object-oriented/conto1.py
@@ -1,10 +1,25 @@
-class ContoCorrente:
+class Conto:
+
     conto = 0
 
-    def __init__(self, nome, saldo=0):
+    def __init__(self, nome, conto = None):
         self.nome = nome
-        ContoCorrente.conto +=1
-        self.conto = f'{ContoCorrente.conto:02d}'
+        if not conto:
+            Conto.conto += 1
+            conto = Conto.conto
+
+        self.conto = f'{conto:02d}'
+
+
+class ContoCorrente(Conto):
+    # conto = 0
+
+    def __init__(self, nome, saldo=0, conto=None):
+
+        super().__init__(nome, conto)
+        # self.nome = nome
+        # ContoCorrente.conto += 1
+        # self.conto = f'{ContoCorrente.conto:02d}'
         self.__saldo = saldo
 
     @property
@@ -26,11 +41,19 @@ class ContoCorrente:
         print(self.nome, self.conto, self.saldo)
 
 
+class GestoreContiCorrenti:
+    @staticmethod
+    def bonifico(sorgente, destinazione, importo):
+        sorgente.preleva(importo)
+        destinazione.deposita(importo)
+
+
 c1 = ContoCorrente("rob", 1000)
 c1.descrizione()
 
 c2 = ContoCorrente("fab", 1500)
 c2.descrizione()
 
-c1.saldo = 10000
+GestoreContiCorrenti.bonifico(c1, c2, 1000)
 c1.descrizione()
+c2.descrizione()
